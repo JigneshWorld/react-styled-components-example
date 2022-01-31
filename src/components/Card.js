7a650777d055d7870546ed8f270452f415a1b455
Jignesh Patel <p.jignesh23@gmail.com>
2022-01-31 14:47:31 +0530

    feat: styled card row and reverse

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
+import { StyledCard } from "./styles/Card.styled";
+
 export default function Card({ item: { id, title, body, image } }) {
     return (
-        <div>
+        <StyledCard layout={id % 2 === 0 && 'row-reverse'}>
             <div>
                 <h2>{title}</h2>
                 <p>{body}</p>
@@ -8,6 +10,6 @@ export default function Card({ item: { id, title, body, image } }) {
             <div>
                 <img src={`./images/${image}`} alt="" />
             </div>
-        </div>
+        </StyledCard>
     )
 }
