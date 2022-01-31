1514dc369ab93ccc03fc38b440cc9a4a7b11bfae
Jignesh Patel <p.jignesh23@gmail.com>
2022-01-31 11:28:21 +0530

    feat: header styled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Header'
 import { Container } from './components/styles/Container.styled'
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Container>
         <h1>Hello World</h1>
       </Container>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { StyledHeader } from './styles/Header.styled'
+
+function Header() {
+    return (
+        <StyledHeader>
+            <h1>Habble</h1>
+        </StyledHeader>
+    );
+}
+
+export default Header;
